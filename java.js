82378bc0a8ca835e69499fc7c1c95fd21b6f630e
yatcamuyucam <yusufatas2002@gmail.com>
2024-01-19 17:09:26 +0300

    codes are formatted

--- java.js
+++ java.js
@@ -1,11 +1,11 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function(){
+window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", this.window.scrollY > 0)
 });
 
-let menu= document.querySelector("#menu-icon");
-let navmenu= document.querySelector("#navmenu");
+let menu = document.querySelector("#menu-icon");
+let navmenu = document.querySelector("#navmenu");
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
